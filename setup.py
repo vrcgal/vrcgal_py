9ed3277dcf98d55c8f041e580bd5b01235981794
Ben Long <14benj@gmail.com>
2017-08-15 05:09:33 -0700

    v0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 
 setup(
     name='vrcgal_py',
-    version='0.1.0',
+    version='0.2.0',
     packages=['vrcgal_py'],
     license='MIT',
     url='https://github.com/vrcgal/vrcgal_py',
