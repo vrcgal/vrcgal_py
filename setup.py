8043cc3f9b857edf88cc6eb58c1c733d4bef27cd
Ben Long <14benj@gmail.com>
2017-08-14 19:52:47 -0700

    pypi cleanup clear

--- setup.py
+++ setup.py
@@ -5,9 +5,12 @@ from distutils.core import setup
 
 
 setup(
-    name='VRCGAL Py',
+    name='vrcgal_py',
     version='0.1.0',
     packages=['vrcgal_py'],
     license='MIT',
+    url='https://github.com/vrcgal/vrcgal_py',
+    author='Ben Long',
+    author_email="14benj@gmail.com",
     long_description=open('README').read()
 )
